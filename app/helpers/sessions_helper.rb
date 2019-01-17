d2c63fad6fbe087376425745a13c570ee87acac5
Koji Yamamuro <iq1ooo.midori@gmail.com>
2019-01-17 22:14:03 +0900

    Add login,logout#5

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,16 @@
+module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @errors = "ああああ#{session[:user_id]}"
+  end
+
+  def logged_in?
+
+    @errors = session[:user_id]
+    return !session[:user_id].nil?
+  end
+end
